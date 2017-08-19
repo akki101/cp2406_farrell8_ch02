4427ce0a541f7e59881bc4fa95476dff6bb5af76
Akki <ankitjakhar4@gmail.com>
2017-08-20 07:24:54 +1000

    initial commint

---
+++ Exercise/src/QuartsToGallons.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class QuartsToGallons {
+    public static void main(String []arg){
+        final int QUARTS_IN_GALLON = 4;
+        int numGallon, numQuarts;
+        String quartsNeeded;
+        quartsNeeded = JOptionPane.showInputDialog("Enter the amount of paint needed(in quarts):");
+        numGallon = Integer.parseInt(quartsNeeded)/QUARTS_IN_GALLON;
+        numQuarts = Integer.parseInt(quartsNeeded)%QUARTS_IN_GALLON;
+        JOptionPane.showMessageDialog(null,"You will be needing "+ numGallon+" gallon and "+ numQuarts+" quarts.");
+
+
+    }
+}
